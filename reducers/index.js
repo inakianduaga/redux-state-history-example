cf94ee2e3fd62e6b0002f32489c3d508e4b6cf9e
Inaki Anduaga <inaki.anduaga@tado.com>
2015-12-20 10:41:55 +0100

    Undo adding unnecessary files

--- reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import todos from './todos'
-
-const rootReducer = combineReducers({
-  todos,
-  stateHistory: (state = {}, action) => state
-})
-
-export default rootReducer
