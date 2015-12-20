a38ab7183a75394a087b31df1a397fffc1655beb
Inaki Anduaga <inaki.anduaga@tado.com>
2015-12-20 10:34:13 +0100

    More fixes in deploy script

--- sync-gh-pages.sh
+++ sync-gh-pages.sh
@@ -6,9 +6,11 @@ set -e
 export GIT_COMMITTER_EMAIL="inaki@inakianduaga.com"
 export GIT_COMMITTER_NAME="Inaki Anduaga"
 
+ls -la ./
 git add -f ./build                     # gh-releases /build folder is not on master branch, so we add it (forced because it's on gitignore usually)
 git stash                              # stash files before switching branches
 git checkout gh-pages
+rm -rf ./build                         # Avoid merge conflicts
 git stash apply --index                # override gh-pages build folder
 git commit -m 'Sync master build'
 git push                               # push to github
